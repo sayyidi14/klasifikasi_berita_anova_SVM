5dd55c9a0c714c1ac603ef9f11d7c143f0a17a7d
sayyidi14 <111933942+sayyidi14@users.noreply.github.com>
2024-06-26 19:45:11 +0700

    Update news_classification.py

--- news_classification.py
+++ news_classification.py
@@ -6,7 +6,6 @@ import nltk
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 import nltk
-nltk.download('punkt')
 import swifter
 import joblib
 import pandas as pd
@@ -31,7 +30,7 @@ def casefoldingText(berita):
         return berita
 
 
-
+nltk.download('punkt')
 
 def word_tokenize_wrapper(text):
     return word_tokenize(text)
